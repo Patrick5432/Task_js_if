6a16ade047241817a742348c9d1b5f1db3a01c73
Maxim <maxx5326@gmail.com>
2023-02-06 12:24:52 +0500

    Добавление новых файлов с новой темой "или" и "и"

---
+++ task_1_or_and.js
@@ -0,0 +1,15 @@
+let age = prompt('Введите число', 0)
+
+if (!(age >= 14 && age <= 90)) {
+    alert('Не входит')
+} else {
+    alert('Входит')
+}
+
+let age_2 = prompt('Введите число', 0)
+
+if (age_2 >= 14 && age_2 <= 90) {
+    alert('Входит')
+} else {
+    alert('Не входит')
+}
---
+++ task_2_or_and.js
@@ -0,0 +1,19 @@
+let login = prompt('Введите логин', '')
+
+if (login === '' || login === null) {
+    alert('Отменено')
+} else if (login === 'Админ') {
+    
+    let password = prompt('Введите пароль', '')
+
+    if (password === 'Я главный') {
+        alert('Здравсвуйте!')
+    } else if (password === '' || password === null) {
+        alert('Отмненено')
+    } else {
+        alert('Неверный пароль')
+    }
+
+} else {
+    alert('Я вас не знаю')
+}
